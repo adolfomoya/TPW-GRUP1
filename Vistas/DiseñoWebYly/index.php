2c072479cada917c67d5346000210b0d1486a671
yulspak <8quispe7@gmail.com>
2020-12-15 09:20:05 -0500

    agreguécontenidoYuls

--- Vistas/DiseñoWebYly/index.php
+++ Vistas/DiseñoWebYly/index.php
@@ -1,3 +1,12 @@
-<?php
-    echo "Yuls";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DiseñoGráfico</title>
+</head>
+<body>
+    <p>yuly, me falta agregar mas contenido :P</p>
+    <img src="img/tupac logo.png" alt="">
+</body>
+</html>
