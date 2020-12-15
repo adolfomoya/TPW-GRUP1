78ca22659f1e93a4375725742c6a658208c288d8
RAUL <raulcamala99@gmail.com>
2020-12-15 09:02:24 -0500

    animacion de graficos

---
+++ Vistas/AnimacionGrafiV/AnimacionG.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animacion de Graficos</title>
+</head>
+<body>
+    <h1>ANIMACION DE GRAFICOS</h1>
+</body>
+</html>
