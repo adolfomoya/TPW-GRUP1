71ab1bf8be6b2d44279c03e2618876a87afbd682
yborda <yulissabordacabrera@gmail.com>
2020-12-15 21:14:40 -0500

    actualizando liderazgo empresarial por yulissa

---
+++ Vistas/LiderazgoEmpresarialYuli/index.php
@@ -0,0 +1,16 @@
+<!-- Hecho por Yulissa Borda-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liderazgo Empresarial</title>
+</head>
+<body>
+    <h1>Curso de Liderazgo Empresarial</h1>
+    <ol>
+        <li>Liderar equipos exitosos es cuestión de actitud. Lleva tu empresa a otro nivel con las herramientas que te ofrece el Curso de Liderazgo Empresarial. Consigue usuarios felices, mantén a tu equipo interesado y conviértete en un líder increíble</li>
+        <li>lala</li>
+    </ol>
+</body>
+</html>
