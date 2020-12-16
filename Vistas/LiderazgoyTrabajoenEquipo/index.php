124da6e29651af4fdc75cf411bee26212339ff90
yborda <yulissabordacabrera@gmail.com>
2020-12-15 22:37:36 -0500

    actualizando yborda

--- Vistas/LiderazgoyTrabajoenEquipo/index.php
+++ Vistas/LiderazgoyTrabajoenEquipo/index.php
@@ -9,11 +9,10 @@
 <body>
     <h1>Curso de Liderazgo y Trabajo en Equipo</h1>
     <ol>
-        <li>El liderazgo empresarial consiste en la habilidad o proceso por medio del cual el 
-            líder de la empresa es capaz de influir en los demás para poder conseguir los objetivos 
-            y satisfacer las necesidades de la empresa. Busca desarrollar el máximo potencial en 
-            empresa y trabajadores. Por eso, las empresas ya no solo necesitan jefes sino también líderes.</li>
-        <li>Características del liderazgo empresarial</li>
+        <p>El liderazgo ha sido definido como la “actividad de influenciar a la gente para que se empeñe voluntariamente en el logro de los objetivos del grupo”. Por grupo debe entenderse un grupo pequeño, un sector de la organización, una organización, etc. Debido a que lo que aquí interesa es el liderazgo en el terreno organizacional, de ahora en más utilizaremos la palabra “organización” para significarla tomada en conjunto o cualquier sector o grupo que la compone.
+            El liderazgo es el proceso de realización de un grupo de personas, convirtiéndolo en un equipo que genera resultados.</p>
+        
     </ol>
+    <h3>Características del liderazgo empresarial</h3>
 </body>
 </html>
