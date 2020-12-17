506e566bfdac12e70340bd564adef93778b201b1
IrvingCcolqque <iccolqque.171ci07@istta.edu.pe>
2020-12-17 08:17:06 -0500

    Irving

--- Vistas/HerramientasMulIr/index.php
+++ Vistas/HerramientasMulIr/index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HerramientaMultimedia</title>
+</head>
+<body>
+    <h1>Herramienta Multimedia</h1>   
+
+
+</body>
+</html>
+
+
 <?php
     echo "Hola";
 ?>
