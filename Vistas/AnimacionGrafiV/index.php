9ddafcde8250fdb82088c17d842b641bd4bd0aa8
Raulz99 <raulcamala99@gmail.com>
2020-12-16 21:56:30 -0500

    animacion de graficos

--- Vistas/AnimacionGrafiV/index.php
+++ Vistas/AnimacionGrafiV/index.php
@@ -9,6 +9,11 @@
     <h1>ANIMACION DE GRAFICOS</h1>
     <ol>
         <li>Definicion</li>
+        <p> La animacion es un proceso utilizado para dar sensacion de movimiento a imagenes o dibujos. Para
+            realizar animaciones existen numerosas tecnicas que van mas alla de los familiares dibujos animados.
+            Los cuadrados se pueden generar  dibujando, pintando o fotografiando los minusculos cambios hechos 
+            repetidamente a un modelo de la realidad o a un modelo tridimencional virtual.</p>
+        
         <li>Tipos de animacion</li>
     </ol>
 </body>
