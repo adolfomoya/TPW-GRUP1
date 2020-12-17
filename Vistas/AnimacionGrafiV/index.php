73e0a9af2e7079f5f98fb4e2b4a2952afa459560
Raulz99 <raulcamala99@gmail.com>
2020-12-16 23:56:30 -0500

    Animacion de graficos_Raul

--- Vistas/AnimacionGrafiV/index.php
+++ Vistas/AnimacionGrafiV/index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Animacion de Graficos</title>
+    <link rel="stylesheet" href="../../css/animacion.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
     <h1>ANIMACION DE GRAFICOS</h1>
+    <a href="../../index.php"><button><img src="../../img/back.png" class="atras"></button></a>
     <ol>
         <li>Definicion</li>
         <p> La animacion es un proceso utilizado para dar sensacion de movimiento a imagenes o dibujos. Para
@@ -15,6 +19,30 @@
             repetidamente a un modelo de la realidad o a un modelo tridimencional virtual.</p>
         
         <li>Tipos de animacion</li>
+        <ul>
+            <li>Dibujos Animados</li>
+            <li>Stop-Motion</li>
+            <li>Pixelacion</li>
+            <li>Rotoscopia</li>
+        </ul>
+        <li>Software de Animacion</li>
+        <div class="container">
+            <div class="tarjet">
+                <img src="../../img/blender.png" width="100px">
+                <br><span>Blender</span>
+            </div>
+            <div class="tarjet">
+                <img src="../../img/cinema4d.png" width="100px">
+                <br><span>Cinema 4D</span>
+            </div>
+            <div class="tarjet">
+                <img src="../../img/animate.png" width="100px">
+                <br><span>Animate</span>
+            </div>
+        </div>
     </ol>
 </body>
+<footer>
+    <span class="fot">© Victor Raul Camala Quispe</span>
+</footer>
 </html>
