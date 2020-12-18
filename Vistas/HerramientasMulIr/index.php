49bec185f8baf220720d34512e131caeb7a696a2
IrvingCcolqque <iccolqque.171ci07@istta.edu.pe>
2020-12-18 09:54:09 -0500

    version 4 irving

--- Vistas/HerramientasMulIr/index.php
+++ Vistas/HerramientasMulIr/index.php
@@ -3,16 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/herramienta.css">
+    
     <title>HerramientaMultimedia</title>
 </head>
 <body>
     <h1>Herramienta Multimedia</h1>   
+    <ol>
+        <li>Introducción</li>
+        <div class="container">
+        <p>Qué es Multimedia?<br>
+            Es cualquier combinación de texto, arte gráfico, sonido, animación y vídeo que llega a nosotros por computadora u otros medios electrónicos. Es un tema presentado con lujos de detalles. </p>  
+        </div>
+        
+        <li>Definición</li>
+        <div class="container">
+        <p>La multimedia consiste en el uso de diversos tipos de medios para transmitir, administrar o presentar información. Estos medios pueden ser texto, gráficas, audio y video, entre otros. Cuando se usa el término</p>  
+        </div>
 
+        <li>Objetivo</li>
+        <div class="container">
+        <p>Desarrollar habilidades en el uso de dispositivos como, video beam y cámaras digitales.
+            <br></br>
+            ACTIVIDADES:
+            <ul>
+                <li>Tomar una foto de cada integrante del grupo y una del grupo completo.</li>
+                <li>Realizar un video de los integrantes del grupo.</li>
+            </ul></p>  
+        </div>
+
+        <li>Elementos Multimedia</li>
+        <div class="container">
+        <ul>
+            <li>Hardware</li>
+            <li>Software</li>
+        </ul>
+        </div>
+    </ol>
 
 </body>
+<footer>
+<a href="../../index.php"><button><img src="../../img/return.jpg" class="return"></button></a>
+    <address>
+        Irving Ccolqque<br>
+        darkrad82gonew@gmail.com
+    </address>
+</footer>
 </html>
-
-
-<?php
-    echo "Hola";
-?>
