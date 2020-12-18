62fe92cb9a14eda0097c746be1f7b02a88f409e9
yborda <yulissabordacabrera@gmail.com>
2020-12-18 15:11:09 -0500

    actualizacion final yulissa

--- Vistas/LiderazgoyTrabajoenEquipo/index.php
+++ Vistas/LiderazgoyTrabajoenEquipo/index.php
@@ -1,18 +1,109 @@
-<!-- Hecho por Yulissa Borda-->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LiderazgoyTrabajoenEquipo</title>
-</head>
-<body>
-    <h1>Curso de Liderazgo y Trabajo en Equipo</h1>
-    <ol>
-        <p>El liderazgo ha sido definido como la “actividad de influenciar a la gente para que se empeñe voluntariamente en el logro de los objetivos del grupo”. Por grupo debe entenderse un grupo pequeño, un sector de la organización, una organización, etc. Debido a que lo que aquí interesa es el liderazgo en el terreno organizacional, de ahora en más utilizaremos la palabra “organización” para significarla tomada en conjunto o cualquier sector o grupo que la compone.
-            El liderazgo es el proceso de realización de un grupo de personas, convirtiéndolo en un equipo que genera resultados.</p>
-        
-    </ol>
-    <h3>Características del liderazgo empresarial</h3>
-</body>
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>LiderazgoyTrabajoenEquipo</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="#" class="image avatar"><img src="images/yo.jpg" alt="50%" /></a>
+					<h1><strong style="color:black">Hola, Bienvenido al curso de Liderazgo y trabajo en Equipo</strong><br />
+				</div>
+			</header>
+
+		<!-- Main -->
+			<div id="main">
+
+				<!-- One -->
+					<section id="one">
+						<header class="major">
+							<h2 >Curso de Liderazgo y Trabajo en Equipo<br />
+						</header>
+						<p><h3>liderazgo ha sido definido como la “actividad de influenciar a la gente para que se empeñe voluntariamente en el logro de los objetivos del grupo”. Por grupo debe entenderse un grupo pequeño, un sector de la organización, una organización, etc. Debido a que lo que aquí interesa es el liderazgo en el terreno organizacional, de ahora en más utilizaremos la palabra “organización” para significarla tomada en conjunto o cualquier sector o grupo que la compone.
+							El liderazgo es el proceso de realización de un grupo de personas, convirtiéndolo en un equipo que genera resultados.</h3></p>
+					</section>
+				<!-- Two -->
+				<h2><strong style="color:rgb(4, 138, 67)">Informacion sobre el Curso</strong></h2>
+					<section id="two">
+						<div class="row">
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/7.jpg" class="image fit thumb"><img src="images/7.jpg" alt="" /></a>
+								<h3><strong style="color:rgb(4, 138, 67)" >COMPETENCIA GENERAL DE LA CARRERA PROFESIONAL</strong></h3>
+								<p><h4>Planificar, organizar y realizar servicios técnicos de enfermería en la atención integral de la persona,
+									familia y comunidad, considerando los niveles de prevención en salud según grado de dependencia y
+									complejidad, con calidad, calidez y actitud ética cumpliendo las normas de bioseguridad y protocolos
+									establecidos.</h4></p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/3.jpg" class="image fit thumb"><img src="images/3.jpg" alt="" /></a>
+								<h3><strong style="color:rgb(4, 138, 67)" >CAPACIDAD TECNICO O ESPECIFICA</strong> </h3>
+								<p><h4> módulo transversal de Liderazgo y Equipo de trabajo desarrolla capacidades para establecer metas,
+									evaluar riesgos, negociar, trabajar en equipo, resolver conflictos y tomar decisiones, practicar valores
+									como la honestidad, respeto, justicia, equidad, responsabilidad, solidaridad; así como propiciar la
+									práctica del asertividad, proactividad, iniciativa, emprendimiento y el compartimiento netico y liderazgo
+									y trabajo en equipo.</h4></p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/2.jpg" class="image fit thumb"><img src="images/2.jpg" alt="" /></a>
+								<h3><strong style="color:rgb(4, 138, 67)" >REQUISITOS DE APROBACIÓN</strong></h3>
+								<p> <li>Nota mínima, según la Escala vigesimal es de TRECE (13). En todos los casos la fracción 0,5 o más se
+									considera como una unidad a favor del estudiante.
+									</li> 
+									<li>
+										Tienen derecho a recuperación previa retroalimentación los estudiantes que hayan obtenido notas
+										entre 10 y 12.
+									</li>
+									<li>
+										Si el estudiante obtuviera nota menor a 10, repite la Unidad Didáctica.
+									</li>
+									<li>
+										El estudiante que acumulara inasistencias injustificadas en número igual mayor al 30%, será
+										desaprobado en forma automática, anotándose en el registro y acta la nota 00, y en observaciones
+										colocar DPI (Desaprobado por inasistencias).
+									</li>
+								</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/4.jpg" class="image fit thumb"><img src="images/4.jpg" alt="" /></a>
+								<h3><strong style="color:rgb(4, 138, 67)" >CAPACIDAD DEL MODULO TRANSVERSAL</strong></h3>
+								<p><h4>Conocer, respetar y promover la ética en su contexto social y laboral. Viabilizar la comunicación dentro
+									de una organización utilizando técnicas para recibir y emitir instrucciones, información, intercambio de
+									idea u opinión de acuerdo a los tipos de flujos de comunicación.</h4></p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/5.jpg" class="image fit thumb"><img src="images/5.jpg" alt="" /></a>
+								<h3><strong style="color:rgb(4, 138, 67)" >PRINCIPALES TEORÍAS SOBRE LIDERAZGO</strong></h3>
+								<p><li>Estilos de Liderazgo y sus efectos en los colaboradores.</li>
+									<li>Tipos de Liderazgo.</li>
+									<li>Liderazgo Ejemplar y Liderazgo Transformador.</li>
+								</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/6.jpg" class="image fit thumb"><img src="images/6.jpg" alt="" /></a>
+								<h3><strong style="color:rgb(4, 138, 67)" >LA INFLUENCIA DEL LÍDER EN LA MOTIVACIÓN DEL PERSONAL</strong></h3>
+								<p><li>Teorías modernas de motivación.</li>
+									<li>Liderazgo y la inteligencia emocional</li>
+									<li>Identificación de factores que  motivan al personal</li>
+								</p>
+							</article>
+						</div>
+						<ul class="actions">
+							<li><a href="../../index.php" class="button">Atrás</a></li>
+						</ul>
+					</section>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
 </html>
