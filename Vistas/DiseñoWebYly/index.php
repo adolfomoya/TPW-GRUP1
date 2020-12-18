e91e1435dec063a8f6028fc98dbb69ee2f1cbe75
yulspak <8quispe7@gmail.com>
2020-12-17 22:03:24 -0500

    yuly diseñoweb

--- Vistas/DiseñoWebYly/index.php
+++ Vistas/DiseñoWebYly/index.php
@@ -4,10 +4,55 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/bootstrap.css">
     <title>DiseñoGráfico</title>
+    <style>
+        h1{
+            padding: 10px 10px;            
+            text-align: center;
+            color:rgb(90, 6, 139);
+        }
+        ol{
+            margin: 50px 50px;
+            color: violet;
+            font-size: 15px;
+        }
+        p{
+            color: whitesmoke;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            margin: 15px;
+            text-align: justify;
+        }        
+
+    </style>
 </head>
-<body>
-    <p>yuly, me falta agregar mas contenido :P</p>
-    <img src="img/tupac logo.png" alt="">
+
+<body style="background-color: gray; font-family:'Roboto', sans-serif; color: white;">
+    <h1>Diseño Web</h1>    
+    <ol>
+        <li>¿Qué es?</li>
+        <p> El diseño web es un área enfocada en el desarrollo de interfaces digitales, como el diseño de sitios y aplicaciones para web. Para ello, los diseñadores web crean las páginas utilizando lenguajes de marcado como HTML.</p>
+        <p>El diseño web implica trabajo relacionado con el layout y diseño de páginas online, así como la producción de contenido, aunque generalmente se aplica a la creación de sitios web.
+        En este caso, los diseñadores web crean las páginas utilizando lenguajes de marcado como HTML o XML.
+        Por otro lado, la parte visual de los sitios está a cargo del CSS, término utilizado para estilizar los elementos escritos en HTML.
+        Por lo tanto, es común que los diseñadores web utilicen ambos para construir un site porque juntos definirán cómo aparecerán las páginas en los navegadores.
+        El proceso de creación de sitios se puede realizar desde herramientas como Adobe Dreamweaver (que requerirá que el profesional sepa más sobre los códigos) y CMS que son plataformas de administración de contenido.
+        </p>
+        <li>¿Qué hace un diseñador web?</li>
+        <p>Los diseñadores web tienen en su trabajo tareas rutinarias relacionadas con la creación de un diseño para medios digitales y la creación de contenido.</p>
+        </ul>
+        <li>Framework</li>
+        <div class="container">
+            <div class="tarjet"><br>                
+                <img src="../../img/Bootstrap-logo.png">
+                <p>Es una biblioteca multiplataforma o conjunto de herramientas de código abierto para diseño de sitios y aplicaciones web.
+                Es de código abierto y de uso gratuito, pero presenta numerosas plantillas HTML y CSS para elementos de la interfaz de usuario, como botones y formularios. Bootstrap también admite extensiones de JavaScript .
+                </p>
+
+            </div>
+            
+        </div>
+        <a href="../../index.php"><button>Regresar</button></a>
+    </ol>
 </body>
 </html>
