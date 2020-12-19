5c51a929e1bae619f207672ee3349b83544ffcf8
rollysihan1 <ccasaquisperolly2@gmail.com>
2020-12-19 10:51:15 -0500

    commit

--- Vistas/ProyectoEmpresarial/index.php
+++ Vistas/ProyectoEmpresarial/index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Proyecto Empresarial</title>
-        <link rel="stylesheet" href="estilos.css">
+        <link rel="stylesheet" href="empresarial.css">
     </head>
     <body>
         <header> 
@@ -14,12 +14,6 @@
 
 
 
-
-            <center>
-                <div>
-                    <h2 class="t1">Galeria de Taller de Programacion Web</h2>
-                </div>
-            </center>
            
         </header>
        
