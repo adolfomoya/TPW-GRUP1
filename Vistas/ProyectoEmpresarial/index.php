5035667735c4838002e00e4a2918383b95b2d1d4
rollysihan1 <ccasaquisperolly2@gmail.com>
2020-12-19 12:29:16 -0500

    commit

--- Vistas/ProyectoEmpresarial/index.php
+++ Vistas/ProyectoEmpresarial/index.php
@@ -2,151 +2,61 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Title</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="../../css/empresarial.css">
-    
   </head>
   <body>
         <header class="titulo">
             <h1 >PROYECTO EMPRESARIAL</h1>
         </header>
         <div class="container">
-        <div class="row " >
-            <div class="col-lg-12 d-flex justify-content-center">
-              <h2>Bienvenida a la Unidad didáctica de Proyecto Empresarial.</h2>
+            <div class="row">
+                <div class="col-xs-1-12 ">
+                    <div class="card " style="width: 18rem;">
+                        <img src="../../img/doncente.png" class="card-img-top" alt="...">
+                        <div class="card-body div" style="border:red 3px dotted;">
+                            <p class="card-text">
+                            Docente:<strong>Edward Walter Pinares Bonnett</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-1-12">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-1-12">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         </div>
-
-        <div class="row">
-
-           
-            <div class="col-lg-5 col-md-6 col-sm-12 ">
-                <div class="card  text-center text-white bg-primary mb-3 ">
-                <img class="card-img-top" src="../../img/empresa.jpg" width="10px" alt="">
-                    <div class="card-body">
-                        <h3 class="justify-content-center " style="font-size: 20px;">Bienvenida</h3>
-                        <p class="card-text borde" >Para el presente ciclo tiene que cumplir 
-                            con la lectura de los materiales, tareas, evaluaciones y participación 
-                            en los foros que forman parte de la medición de tu logro de aprendizaje.</p>
-                    </div>
-                    <div class="card-footer text-white bg-danger">
-                        EDWUAR WALTER PINARES BONNETT
-                    </div> 
-               </div>
-           </div>
-           <div class="col-lg-4 col-md-6 col-sm-12 order-4">
-                <div class="card text-center text-white bg-warning mb-3 ">
-                    <img class="card-img-top" src="t5.png" alt="">
-                    <div class="card-body">
-                        <h3 class="card-title">Formulario con Arrays PHP</h3>
-                        <p class="card-text">Tarea Numero 5</p>
-                    </div>
-                    <div class="card-footer text-white bg-danger">
-                        Rolly Ccasa
-                    </div> 
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 order-5">
-                <div class="card text-center text-white bg-success mb-3 ">
-                    <img class="card-img-top" src="t6.png" alt="">
-                    <div class="card-body">
-                        <h3 class="card-title">Sesiones y Formulario PHP</h3>
-                        <p class="card-text">Tarea Numero 6</p>
-                    </div>
-                    <div class="card-footer text-white bg-danger">
-                        Rolly Ccasa
-                    </div> 
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 order-6">
-                <div class="card text-center text-white bg-danger mb-3 ">
-                    <img class="card-img-top" src="t6.png" alt="">
-                    <div class="card-body">
-                        <h3 class="card-title">Formulario y Correo PDO PHP</h3>
-                        <p class="card-text">Tarea Numero 7</p>
-                    </div>
-                    <div class="card-footer text-white bg-warning">
-                        Rolly Ccasa
-                    </div> 
-               </div>
-           </div>
-           <div class="col-lg-4 col-md-6 col-sm-12 order-7">
-                <div class="card text-center text-white bg-success mb-3 ">
-                    <img class="card-img-top" src="t7.png" alt="">
-                    <div class="card-body">
-                        <h3 class="card-title">Aplicando Clases PDO PHP</h3>
-                        <p class="card-text">Tarea Numero 8</p>
-                    </div>
-                    <div class="card-footer text-white bg-warning">
-                        Rolly Ccasa
-                    </div> 
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 order-8">
-                <div class="card text-center text-white bg-info mb-3 ">
-                    <img class="card-img-top" src="t7.png" alt="">
-                    <div class="card-body">
-                        <h3 class="card-title">Abstraccion y Encapsulamineto PDO PHP</h3>
-                        <p class="card-text">Tarea Numero 9</p>
-                    </div>
-                    <div class="card-footer text-white bg-warning">
-                        Rolly Ccasa
-                    </div> 
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 order-9">
-                <div class="card text-center text-white bg-danger mb-3 ">
-                    <img class="card-img-top" src="t10.png" alt="">
-                    <div class="card-body">
-                        <h3 class="card-title">Herencia PDO PHP</h3>
-                        <p class="card-text">Tarea Numero 10</p>
-                    </div>
-                    <div class="card-footer text-white bg-warning">
-                        Rolly Ccasa
-                    </div> 
-               </div>
-           </div>
-           <div class="col-lg-4 col-md-6 col-sm-12 order-10">
-                <div class="card text-center text-white bg-success mb-3 ">
-                    <img class="card-img-top" src="t9.png" alt="">
-                    <div class="card-body">
-                        <h3 class="card-title">Particularizados y Poliformismo PDO PHP</h3>
-                        <p class="card-text">Tarea Numero 11</p>
-                    </div>
-                    <div class="card-footer text-white bg-warning">
-                        Rolly Ccasa
-                    </div> 
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 order-11">
-                <div class="card text-center text-white bg-info mb-3 ">
-                    <img class="card-img-top" src="t8.png" alt="">
-                    <div class="card-body">
-                        <h3 class="card-title">Practica Calificadad PDO PHP</h3>
-                        <p class="card-text">Tarea Numero 12</p>
-                    </div>
-                    <div class="card-footer text-white bg-warning">
-                        Rolly Ccasa
-                    </div> 
-                </div>
-            </div>
-            
-      </div>
-       
-    </div>
        
 
-      
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
   </body>
 </html>
