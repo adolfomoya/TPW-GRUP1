4c87e3c7438a90757096fdbfce8bed17868e54d7
WCALLO <willics0922@gmail.com>
2020-12-18 23:49:38 -0500

    willian Diseño grafico

--- Vistas/DiseñoGraficoW/index.php
+++ Vistas/DiseñoGraficoW/index.php
@@ -1,15 +1,41 @@
-<?php
-    ECHO "Hola soy la página de la U.D. de Diseño Gráfico";
-    ECHO "Este es otro cambio";
-    ECHO "Este es otro cambio";
-    ECHO "Este es otro cambio";
-    ECHO "Este es otro cambio";
-    ECHO "Este es otro cambio";
+<!-- HECHO POR Willian Eugeino Callo Salcedo-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diseño Gráfico</title>
+    <link rel="stylesheet" href="../../css/ddgg.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+<body style="background:url(../../img/dgw.jpg)">
+    <h1 style="color:white">DISEÑO GRÁFICO</h1>
+    <a href="../../index.php"><button><img src="../../img/atrs.png" class="atras"></button></a>
+    <ol>
+        <li style="background-color: rgba(38, 38, 61 ); padding:25px; color:white">DEFINICIÓN</li>
+        <p style="background-color: rgba(38, 38, 61, 0.705); padding:25px; color:white"> El diseño gráfico es una profesión y disciplina académica cuya actividad consiste en proyectar comunicaciones visuales destinadas a transmitir mensajes específicos a grupos sociales, con objetivos determinados. Esta actividad ayuda a optimizar las comunicaciones gráficas.</p>
+        
+        <li style="background-color: rgba(38, 38, 61 ); padding:25px; color:white">¿PARA QUE SIRVE EL DISEÑO GRAFICO???</li>
+        
+        <p style="background-color: rgba(38, 38, 61, 0.705); padding:25px; color:white"> Esta disciplina se sirve de imágenes, vídeos y elementos textuales, todos ellos elementos imprescindibles para la comunicación gráfica y visual. Es, a partir de ellos, como el diseño gráfico consigue trasmitir los mensajes e ideas necesarias.</p>
+        <p style="background-color: rgba(38, 38, 61, 0.705); padding:25px; color:white"> Por ello, la respuesta a «para qué sirve el diseño gráfico» es simple. Es el ámbito que permite a las empresas plasmar sus ideas, mensajes y productos. O dicho de otra manera, es la disciplina que permite transmitir la identidad corporativa de una marca o de un producto.</p>
+        <li style="background-color: rgba(38, 38, 61); padding:25px; color:white">HABILIDADES DEL DISEÑADOR GRÁFICO</li>
+        
+        
+        <ul style="background-color: rgba(38, 38, 61, 0.705); padding:25px; color:white" >
+            <li>Creativo</li>
+            <li>Escucha Activa</li>
+            <li>CompetenciasTecnologicas</li>
+            <li>Versatilidad</li>
+        </ul>
+        
+        
+    </ol>
+</body>
+<footer style="background-color: rgba(38, 38, 61, 0.705); padding:25px; color:white">
+    <span style="background-color: rgba(38, 38, 61, 0.705); padding:25px; color:white">©WillianCallo
 
-    ECHO "Este es otro cambio";
-    ECHO "Este es otro cambio";
-
-    ECHO "Este es otro cambio";ECHO "Este es otro cambio";
-    ECHO "Este es otro cambio";
-    
-?>
+</span>
+</footer>
+</html>
